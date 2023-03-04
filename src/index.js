6ffea260eb0fdf63e71a12994b2c5390524928de
malik_samad <malik.samad@wearenova.co.uk>
2023-03-04 16:01:48 +0500

    finished 02 Assest Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from "lodash"
+import './style.css';
+import icon from "./images/green-tick.png"
+
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add("hello")
+
+    const image = new Image(50, 50);
+    image.src = icon;
+
+    element.appendChild(image);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const CopyPlugin = require( 'copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins:[
+    new CopyPlugin({
+        patterns:[
+           path.resolve(__dirname,"src", "test.txt")
+        ]
+    })
+  ],
+  module: {
+    rules:[
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader",],
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif|jpeg)$/i,
+            type: "asset/resource"
+        }
+    ]
+  }
 };
